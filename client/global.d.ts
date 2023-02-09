0a97463a79cb4a114886ae01dd3c9a116da5f6ac
mcsmsxfm56 <mcuadrado135@gmail.com>
2023-02-09 19:59:51 -0300

    armando la base de firebase

---
+++ client/global.d.ts
@@ -0,0 +1,11 @@
+declare namespace NodeJS {
+  interface ProcessEnv {
+    APIKEY: string;
+    AUTHDOMAIN: string;
+    PROJECTID: string;
+    STORAGEBUCKET: string;
+    MESSAGINGSENDERID: string;
+    APPID: string;
+    MEASUREMENTID: string;
+  }
+}
