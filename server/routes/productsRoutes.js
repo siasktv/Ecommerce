41406178c2e9bf10457d0b34089798371c28900b
siasktv <biankmbm92@gmail.com>
2023-03-06 11:49:56 -0500

    Routes Updated

--- server/routes/productsRoutes.js
+++ server/routes/productsRoutes.js
@@ -4,10 +4,12 @@ const {
   getAllProducts,
   getProductById,
   createProduct,
+  deleteProduct,
 } = require('../controllers/productsControllers.js')
 const { isAdmin } = require('../middleware/auth')
 
 router.get('/', getAllProducts)
 router.get('/:id', getProductById)
+router.delete('/:id', deleteProduct)
 router.post('/createProduct', createProduct)
 module.exports = router
